af93e691eae0f24ac126920e3e5cfacc428dde6b
nasim-saaidi <302263973@student.rocmondriaan.nl>
2023-05-30 17:51:07 +0200

    insert correct files

--- javascript/main.js
+++ javascript/main.js
@@ -20,7 +20,7 @@ function higher() {
         alert('je hebt het goed, je gooide ' + dice + ', plus 5');
         playerOne = playerOne + 5;
         alert('je hebt nu ' + playerOne + ' punten');
-        mainNumber.innerHTML = decidingNumber;
+        
     }
 
 
@@ -28,27 +28,24 @@ function higher() {
         alert('jammer, je hebt het fout, je gooide ' + dice + ', min 5 ');
         playerOne = playerOne - 5;
         alert('je hebt nu ' + playerOne + ' punten');
-        mainNumber.innerHTML = decidingNumber;
-
     }
 
     else if (dice === decidingNumber) {
         alert('je hebt, ' + dice + ' gegooid, dit is gelijk, plus 0');
         alert('je hebt nu ' + playerOne + ' punten');
-        mainNumber.innerHTML = decidingNumber;
-
+       
     };
 
     updateNumber();  
 }
 
 function lower() {
-    let dice = Math.round(Math.random() * 6);
+   
     if (dice > decidingNumber) {
         alert('jammer, je hebt het fout, je gooide ' + dice + ', - 5');
         playerOne = playerOne - 5;
         alert('je hebt nu ' + playerOne + ' punten');
-        mainNumber.innerHTML = decidingNumber;
+
     }
 
 
@@ -56,14 +53,14 @@ function lower() {
         alert('je hebt het goed, je gooide ' + dice + ', plus 5');
         playerOne = playerOne + 5;
         alert('je hebt nu ' + playerOne + ' punten');
-        mainNumber.innerHTML = decidingNumber;
+        
 
     }
     else if (dice === decidingNumber) {
         alert('je hebt ' + dice + ' gegooid, dit is gelijk, plus 0');
         alert('je hebt nu ' + playerOne + ' punten');
-        mainNumber.innerHTML = decidingNumber;
-    };
+        
+    }
 updateNumber();
 }
 
